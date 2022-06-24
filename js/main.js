63bd597282a8f840789f3f6b08a1eb6957ba6927
Jaengtol <alzkehfk@naver.com>
2022-06-24 11:08:39 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('.allBtn').click(function(){
+    $('nav').animate({left:0})
+});
+
+$('nav button').click(function(){
+    $('nav').animate({left:'-100%'})
+});
+
+$(window).resize(function(){
+    if($('nav').removeAttr('style'));
+});
+//nav에 스크립트가 적용 된게 있으면 화면애 resize될때 없애준다
